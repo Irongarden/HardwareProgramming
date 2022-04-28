e69b15073c07af2ebd0b245b487e0332adf09f26
Irongarden <mathiashave@me.com>
2022-04-28 12:03:44 +0200

    yey

---
+++ src/hal/matrix16/include/matrix.h
@@ -0,0 +1,17 @@
+/*
+ * matrix.h
+ *
+ * Created: 28-04-2022 11:17:19
+ *  Author: mathi
+ */ 
+
+
+#ifndef MATRIX_H_
+#define MATRIX_H_
+
+
+void init_matrix_keyboard();
+int8_t getxkey();
+
+
+#endif /* MATRIX_H_ */
---
+++ src/hal/matrix16/src/matrix.c
@@ -0,0 +1,29 @@
+/*
+ * matrix.c
+ *
+ * Created: 28-04-2022 11:17:30
+ *  Author: mathi
+ */ 
+#include "../include/matrix.h"
+
+typedef struct key_descr_t{
+	volatile uint8_t* ddr;
+	volatile uint8_t* pin;
+	volatile uint8_t* port;
+	uint8_t bit;
+	key_descr_t;
+	};
+	
+static key_descr_t key[] = {
+	{}
+		
+	
+};
+
+void init_matrix_keyboard(){
+	
+}
+
+int8_t getxkey(){
+	
+}
