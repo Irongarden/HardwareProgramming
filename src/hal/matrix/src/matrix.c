dab86408687dd90cf86fbd22d6f06e894ee83b3c
Irongarden <mathiashave@me.com>
2022-04-28 11:48:41 +0200

    init

---
+++ src/hal/matrix/src/matrix.c
@@ -0,0 +1,23 @@
+/*
+ * matrix.c
+ *
+ * Created: 28-04-2022 11:17:30
+ *  Author: mathi
+ */ 
+#include "../include/matrix.h"
+
+typedef struct key_descr_t{
+	volatile uint8_t* ddr;
+	volatile uint8_t* pin;
+	volatile uint8_t* port;
+	uint8_t bit;
+	key_descr_t;
+	};
+
+void init_matrix_keyboard(){
+	
+}
+
+int8_t getxkey(){
+	
+}
